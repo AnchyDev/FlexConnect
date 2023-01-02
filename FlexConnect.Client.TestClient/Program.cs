d4ee4fc8729f53006d07e3b4630a1fd943587010
AnchyDev <lewienato@gmail.com>
2023-01-02 18:31:13 +1100

    Add project files.

---
+++ FlexConnect.Client.TestClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FlexConnect.Client.TestClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
