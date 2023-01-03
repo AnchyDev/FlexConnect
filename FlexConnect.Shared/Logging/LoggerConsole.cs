2cc87bc8c58dbd1a348fc7f5cb40399e597de47a
AnchyDev <lewienato@gmail.com>
2023-01-03 13:58:29 +1100

    Cleanup unused usings.

--- FlexConnect.Shared/Logging/LoggerConsole.cs
+++ FlexConnect.Shared/Logging/LoggerConsole.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FlexConnect.Shared.Logging
+﻿namespace FlexConnect.Shared.Logging
 {
     public class LoggerConsole : ILogger
     {
