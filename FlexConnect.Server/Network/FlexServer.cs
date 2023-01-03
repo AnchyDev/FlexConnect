b373d42c2be996b1e61be9fb3a5be8455be39211
AnchyDev <lewienato@gmail.com>
2023-01-03 20:07:22 +1100

    Moved handshake to its own method.

--- FlexConnect.Server/Network/FlexServer.cs
+++ FlexConnect.Server/Network/FlexServer.cs
@@ -1,5 +1,6 @@
 ﻿using FlexConnect.Shared.Logging;
 using FlexConnect.Shared.Network;
+
 using System.Net;
 using System.Net.Sockets;
 
@@ -60,46 +61,53 @@ namespace FlexConnect.Server.Network
         {
             try
             {
-                // Handshake
+                if(!await HandshakeAsync(tcpClient))
                 {
-                    await _logger.LogAsync(LogLevel.Info, $"Client '{tcpClient.Client.RemoteEndPoint}' connected.");
-
-                    byte[] payload = Guid.NewGuid().ToByteArray();
-
-                    var packet = new PacketBuilder(OpCode.Auth)
-                        .Append<int>(payload.Length)
-                        .Append<byte[]>(payload)
-                        .Build();
-
-                    await PacketHandler.SendAsync(tcpClient.GetStream(), packet);
-
-                    var opCode = await PacketHandler.ReadOpCodeAsync(tcpClient.GetStream());
-
-                    if (opCode != OpCode.Auth)
-                    {
-                        await _logger.LogAsync(LogLevel.Error, $"Client '{tcpClient.Client.RemoteEndPoint}' tried sending opcode {opCode} instead of {OpCode.Auth} before Auth was complete. Disconnecting.");
-                        await DisconnectUser(tcpClient);
-                        return;
-                    }
-
-                    var lenBytes = await PacketHandler.ReadAsync<int>(tcpClient.GetStream());
-                    var len = BitConverter.ToInt32(lenBytes);
-                    var responsePayload = await PacketHandler.ReadAsync<byte[]>(tcpClient.GetStream(), len);
-
-                    if(new Guid(payload).CompareTo(new Guid(responsePayload)) != 0)
-                    {
-                        await _logger.LogAsync(LogLevel.Error, $"Client '{tcpClient.Client.RemoteEndPoint}' failed to match handshake guid. Disconnecting.");
-                        await DisconnectUser(tcpClient);
-                        return;
-                    }
-
-                    await _logger.LogAsync(LogLevel.Debug, $"Client '{tcpClient.Client.RemoteEndPoint}' passed handshake.");
+                    await _logger.LogAsync(LogLevel.Error, $"Client '{tcpClient.Client.RemoteEndPoint}' failed handshake. Disconnecting.");
+                    await DisconnectUser(tcpClient);
                 }
             }
             catch(Exception ex)
             {
                 await _logger.LogAsync(LogLevel.Error, $"{ex}");
             }
+        }
+
+        private async Task<bool> HandshakeAsync(TcpClient tcpClient)
+        {
+            await _logger.LogAsync(LogLevel.Info, $"Client '{tcpClient.Client.RemoteEndPoint}' connected.");
+
+            byte[] payload = Guid.NewGuid().ToByteArray();
+
+            var packet = new PacketBuilder(OpCode.Auth)
+                .Append<int>(payload.Length)
+                .Append<byte[]>(payload)
+                .Build();
+
+            await PacketHandler.SendAsync(tcpClient.GetStream(), packet);
+
+            var opCode = await PacketHandler.ReadOpCodeAsync(tcpClient.GetStream());
+
+            if (opCode != OpCode.Auth)
+            {
+                await _logger.LogAsync(LogLevel.Error, $"Client '{tcpClient.Client.RemoteEndPoint}' tried sending opcode {opCode} instead of {OpCode.Auth} before Auth was complete. Disconnecting.");
+                await DisconnectUser(tcpClient);
+                return false;
+            }
+
+            var lenBytes = await PacketHandler.ReadAsync<int>(tcpClient.GetStream());
+            var len = BitConverter.ToInt32(lenBytes);
+            var responsePayload = await PacketHandler.ReadAsync<byte[]>(tcpClient.GetStream(), len);
+
+            if (new Guid(payload).CompareTo(new Guid(responsePayload)) != 0)
+            {
+                await _logger.LogAsync(LogLevel.Error, $"Client '{tcpClient.Client.RemoteEndPoint}' failed to match handshake guid. Disconnecting.");
+                await DisconnectUser(tcpClient);
+                return false;
+            }
+
+            await _logger.LogAsync(LogLevel.Debug, $"Client '{tcpClient.Client.RemoteEndPoint}' passed handshake.");
+            return true;
         }
 
         private async Task DisconnectUser(TcpClient tcpClient)
